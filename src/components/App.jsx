5f1d0a26e5f7a67ab8b1cd02123b792eb0d4d944
Matias Pierobon <matias@Matiass-iMac.local>
2016-12-23 05:36:00 -0300

    update folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,38 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
 import SideMenu from './SideMenu';
+import Users from './Users'
 import defaultTheme from '../themes/default';
 import './App.css';
 
 const muiTheme = getMuiTheme(defaultTheme);
 
-const App = () => (
-    <MuiThemeProvider muiTheme={muiTheme}>
-        <sid-app>
-            <AppBar
-                title="Dmitri"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-                style={{ position: 'fixed' }}
-            />
-            
-            <SideMenu />
+class App extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {expand: true}
+    }
 
-            <div className="page-content">
-                <Users />
-            </div>
-        </sid-app>
-    </MuiThemeProvider>
-);
+    handleExpand = () => this.setState({expand : !this.state.expand});
+
+    render = () => (
+        <MuiThemeProvider muiTheme={muiTheme}>
+            <app className={[this.state.expand ? "expand" : null]}>
+                <AppBar
+                    onLeftIconButtonTouchTap={this.handleExpand}
+                    title="Dmitri"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    style={{ position: 'fixed' }}
+                />
+                
+                <SideMenu />
+    
+                <div className="page-content">
+                    <Users /> 
+                </div>
+            </app>
+        </MuiThemeProvider>
+    );
+}
 
 export default App;
